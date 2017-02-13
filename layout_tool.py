b99e94fe1b758c06e96b65fa9e175f618d5defed
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-02-13 17:45:26 -0500

    Various layout tool frontend and backend changes
    BACKEND
    - Renames some routes
      add-layout -> upload layout
      create-layout -> add-layout
    
    - Changes (new) add_layout to not take a layout name but create
      a layout with a default timestamped name
    
    - Adds save_layout
      Central function to update the layout using form posted data
    
    - Adds custom context processor to call in_toto's unpack_rule
      function (converts list rule to rule data) from jinja template
    
    - Adds custom template filter to convert an in_toto datetime
      (zulu) to HTML (local-datetime) conformant string
    
    FRONTEND:
    - Adds input fields for all editable content
    - Uses box css class to better separate different enteties
    - Adds new html partial for matchrules

--- layout_tool.py
+++ layout_tool.py
@@ -24,8 +24,14 @@ import sys
 import hashlib
 import random
 import urllib
+import time
+import datetime
+import dateutil.relativedelta
+import dateutil.parser
 
 import in_toto.models.layout
+import in_toto.artifact_rules
+import securesystemslib.keys
 
 from flask import (Flask, render_template, session, redirect, url_for, request,
     flash)
@@ -157,8 +163,9 @@ def show_layout(session_id):
       session_id=session_id, layout=layout, layout_choices=layout_choices,
       layout_name=layout_name)
 
-@app.route("/<md5:session_id>/add-layout", methods=["POST"])
-def add_layout(session_id):
+
+@app.route("/<md5:session_id>/upload-layout", methods=["POST"])
+def upload_layout(session_id):
   """
   <Purpose>
     Adds a posted layout to the session directory and redirects to show
@@ -188,29 +195,119 @@ def add_layout(session_id):
       layout_name=layout_name))
 
 
-@app.route("/<md5:session_id>/create-layout", methods=["POST"])
-def create_layout(session_id):
+@app.route("/<md5:session_id>/add-layout", methods=["POST"])
+def add_layout(session_id):
   """
   <Purpose>
-    Adds a posted public key to the layout
+    Adds a new empty layout with a default name to the session directory.
+    The name is "untitled-<unixtimestamp>.layout" and can be changed later.
+  <Returns>
+    Redirects to show layout page
+
+  """
+  session_path = _session_path(session_id)
+
+  # A timestamped file name seems rather unique (for one session on one machine)
+  layout_name = "untitled-" + str(time.time()).replace(".", "") + ".layout"
+
+  layout = in_toto.models.layout.Layout()
+  # FIXME: Moving default setup to the layout constructor would be nicer
+  # Cf. https://github.com/in-toto/in-toto/issues/36
+  layout.expires = (datetime.datetime.today()
+      + dateutil.relativedelta.relativedelta(months=1)
+      ).isoformat() + 'Z'
+
+  layout_path = os.path.join(session_path, layout_name)
+  layout.dump(layout_path)
+  flash("Successfully created new layout '{}'".format(layout_name))
+
+  return redirect(url_for("show_layout", session_id=session["id"],
+      layout_name=layout_name))
+
+
+@app.route("/<md5:session_id>/save-layout", methods=["POST"])
+def save_layout(session_id):
+  """
+  <Purpose>
+    Update layout with posted arguments.
+    Each time re-sends and re-writes the entire layout.
+    In case the file name is changed we delete the old filename.
 
   <Returns>
     Redirects to show layout page
 
   """
   session_path = _session_path(session_id)
-  layout_name = request.form.get("layout_name")
+  # Get new and old layout file name and create session paths
+  old_layout_name = secure_filename(request.form.get("layout_name"))
+  new_layout_name = secure_filename(request.form.get("new_layout_name"))
+  old_layout_path = os.path.join(session_path, old_layout_name)
+  new_layout_path = os.path.join(session_path, new_layout_name)
 
-  if not layout_name:
-    flash("No layout name specified.")
-    return redirect(url_for("show_layout", session_id=session["id"]))
+  # Fetch other form properties
+  expires = request.form.get("expires")
+  pubkey_files = request.files.getlist("pubkey[]")
 
-  layout = in_toto.models.layout.Layout()
-  layout_path = os.path.join(session_path,  secure_filename(layout_name))
-  layout.dump(layout_path)
+  # Load layout from file
+  layout = in_toto.models.layout.Layout.read_from_file(old_layout_path)
+
+  # Set attributes
+  expires_zulu_fmt = dateutil.parser.parse(expires).isoformat() + 'Z'
+  layout.expires = expires_zulu_fmt
+
+  # TODO: It would be nicer if this was done in in-toto, e.g.: something
+  # like in_toto.util.import_rsa_public_keys_from_files_as_dict
+  pubkeys = {}
+  for pubkey_file in pubkey_files:
+    # FIXME: Handle wrong formats
+    rsa_key = securesystemslib.keys.import_rsakey_from_public_pem(
+        pubkey_file.read())
+    pubkeys[rsa_key["keyid"]] = rsa_key
+
+  # FIXME: Should we update or overwrite?
+  if pubkeys:
+    layout.keys = pubkeys
+
+  # If the filename has changed, we can replace the old layout
+  if old_layout_path != new_layout_path:
+    layout.dump(new_layout_path)
+    os.remove(old_layout_path)
+    layout_name = new_layout_name
+  else:
+    layout.dump(old_layout_path)
+    layout_name = old_layout_name
 
   return redirect(url_for("show_layout", session_id=session["id"],
       layout_name=layout_name))
+
+
+@app.context_processor
+def in_toto_processor():
+  def unpack_rule(rule):
+    """
+    <Purpose>
+      Adds in_toto unpack_rule as tag to the template engine, use like:
+      {{ unpack_rule(rule) }}
+
+    <Arguments>
+      rule:
+              In-toto artifact rule in list format
+              cf. in_toto.artifact_rules.unpack_rule
+
+    <Returns>
+      Returns rule_data cf. in_toto.artifact_rules.unpack_rule
+
+    """
+    return in_toto.artifact_rules.unpack_rule(rule)
+  return dict(unpack_rule=unpack_rule)
+
+
+@app.template_filter('zulu_to_html')
+def _zulu_to_html(date):
+  """Converts Zulu datetime object to "yyyy-MM-ddThh:mm as required by
+  HTML widget. """
+  datetime = dateutil.parser.parse(date)
+  return datetime.strftime("%Y-%m-%dT%H:%M")
 
 if __name__ == "__main__":
   app.run()
