85407c1fbfc1b0e64aa660e5f7a939a18a45138e
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-02-14 15:47:05 -0500

    Adds JS to add and remove steps and inspections
    We add a hidden step and inspection form template to the DOM
    and clone/append/show on button click.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-  // Register buttons to clone form widgets
+// Register buttons
+// We do not directly listen on the elements but on the document instead
+// to allow dynamic creation of content (delegation).
+// TODO: We could DRY-up step/inspection/material add and remove
+// They do similar things.
 
+$(document).on("click", "button.add-step", function(evt){
+  $(".step.template")
+    .clone()
+    .removeClass("template")
+    .appendTo("#step-container");
+});
+$(document).on("click", "button.remove-step", function(evt){
+  $(this).closest(".step").remove();
+});
+
+$(document).on("click", "button.add-inspection", function(evt){
+  $(".inspection.template")
+    .clone()
+    .removeClass("template")
+    .appendTo("#inspection-container");
+});
+$(document).on("click", "button.remove-inspection", function(evt){
+  $(this).closest(".inspection").remove();
 });
