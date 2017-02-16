ad68db01caf7875f251211e17155ab2263ee932c
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-02-16 14:53:27 -0500

    Separates listeners from functions in main.js
    Minor file restructuring

--- static/js/main.js
+++ static/js/main.js
@@ -1,56 +1,8 @@
-/*
- * Register click listeners to add and remove dynamic form elements
- *
- * We do not directly listen on the elements but on the document instead
- * to allow dynamic creation of content (delegation).
- *
- * NOTE 1: There are a lot of different ways to achieve below result.
- * The Event-based is a personal preference.
- * In any case this could be more DRY, maybe at the cost of readability?
+
+/* ----------------------------------------------------------------------------
+ * DOM to JSON functions for main layout form
+ * ----------------------------------------------------------------------------
  */
-$(document).on("click", "button.add-step", function(evt) {
-  // Query the template, clone it, remove the property that hid it
-  // and append it to the correct target inside the form.
-  $(".step.template").clone().removeClass("template")
-      .appendTo("#step-container-inner");
-});
-
-$(document).on("click", "button.add-inspection", function(evt) {
-  // Query the template, clone it, remove the property that hid it
-  // and append it to the correct target inside the form.
-  $(".inspection.template").clone().removeClass("template")
-      .appendTo("#inspection-container-inner");
-});
-$(document).on("click", "button.add-rule", function(evt) {
-  // Rule type must be one of
-  // "match", "allow", "disallow", "create", "delete", "modify"
-  var rule_type = $(this).closest(".add-rule-container")
-      .find("option:selected").val();
-
-  // We identify the target by searching up the DOM for the
-  // element that contains the rules and the rule picker (i.e. rule-container),
-  // then we search down for the rule-container-inner that contains only
-  // the rules.
-  var $target = $(this).closest(".rule-container")
-      .find(".rule-container-inner");
-
-  // Finally query the template, clone it, remove the property that hid it
-  // and  append it to the correct target inside the form.
-  $(".rule.template." + rule_type).clone().removeClass("template")
-      .appendTo($target);
-});
-
-// Register click listener to remove dynamic nested form elements
-// Find container element and remove it.
-$(document).on("click", "button.remove-step", function(evt){
-  $(this).closest(".step").remove();
-});
-$(document).on("click", "button.remove-inspection", function(evt){
-  $(this).closest(".inspection").remove();
-});
-$(document).on("click", "button.remove-rule", function(evt){
-  $(this).closest(".rule").remove();
-});
 
 
 /*
@@ -230,6 +182,68 @@ var submit_layout_as_json = function($form, json_data) {
   // But it will be gone when the server returns.
   $ghost_form.appendTo("body").submit();
 }
+
+
+
+
+
+/* ----------------------------------------------------------------------------
+ * Register Event Listener
+ * ----------------------------------------------------------------------------
+ *
+ * Register click listeners to add and remove dynamic form elements
+ *
+ * We do not directly listen on the elements but on the document instead
+ * to allow dynamic creation of content (delegation).
+ *
+ * NOTE 1: There are a lot of different ways to achieve below result.
+ * The Event-based is a personal preference.
+ * In any case this could be more DRY, maybe at the cost of readability?
+ */
+
+$(document).on("click", "button.add-step", function(evt) {
+  // Query the template, clone it, remove the property that hid it
+  // and append it to the correct target inside the form.
+  $(".step.template").clone().removeClass("template")
+      .appendTo("#step-container-inner");
+});
+
+$(document).on("click", "button.add-inspection", function(evt) {
+  // Query the template, clone it, remove the property that hid it
+  // and append it to the correct target inside the form.
+  $(".inspection.template").clone().removeClass("template")
+      .appendTo("#inspection-container-inner");
+});
+$(document).on("click", "button.add-rule", function(evt) {
+  // Rule type must be one of
+  // "match", "allow", "disallow", "create", "delete", "modify"
+  var rule_type = $(this).closest(".add-rule-container")
+      .find("option:selected").val();
+
+  // We identify the target by searching up the DOM for the
+  // element that contains the rules and the rule picker (i.e. rule-container),
+  // then we search down for the rule-container-inner that contains only
+  // the rules.
+  var $target = $(this).closest(".rule-container")
+      .find(".rule-container-inner");
+
+  // Finally query the template, clone it, remove the property that hid it
+  // and  append it to the correct target inside the form.
+  $(".rule.template." + rule_type).clone().removeClass("template")
+      .appendTo($target);
+});
+
+// Register click listener to remove dynamic nested form elements
+// Find container element and remove it.
+$(document).on("click", "button.remove-step", function(evt){
+  $(this).closest(".step").remove();
+});
+$(document).on("click", "button.remove-inspection", function(evt){
+  $(this).closest(".inspection").remove();
+});
+$(document).on("click", "button.remove-rule", function(evt){
+  $(this).closest(".rule").remove();
+});
 
 /*
  * Register layout form submit listener to intercept vanilla form post
