87377e572712344c42282d992b6c01117aa0e5cb
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-04-06 16:25:49 -0400

    Adds basic view and template for every wizard page

--- wizard.py
+++ wizard.py
@@ -36,6 +36,92 @@ app.config.update(dict(
 # e.g. your deployment secret key
 app.config.from_pyfile("config.py")
 
+@app.route("/")
+def start():
+  """Step 0.
+  Wizard entry point, static landing page. """
+  return render_template("start.html")
+
+
+@app.route("/versioning")
+def versioning():
+  """Step 1.
+  Enter information about version control system. """
+  return render_template("versioning.html")
+
+
+@app.route("/building")
+def building():
+  """Step 2.
+  Enter information about building. """
+  return render_template("building.html")
+
+
+@app.route("/quality")
+def quality_management():
+  """Step 3.
+  Enter information about quality management. """
+  return render_template("quality.html")
+
+
+@app.route("/packaging")
+def packaging():
+  """Step 4.
+  Enter information about packaging. """
+  return render_template("packaging.html")
+
+
+@app.route("/software-supply-chain")
+def software_supply_chain():
+  """Step 5.
+  Visualize and edit software supply chain. """
+  return render_template("software_supply_chain.html")
+
+
+@app.route("/authorizing")
+def authorizing():
+  """Step 6.
+  Functionary keys upload and keys dropzone. """
+  return render_template("authorizing.html")
+
+
+@app.route("/authorizing/upload")
+def ajax_upload_key():
+  """Ajax upload functionary keys. """
+  pass
+
+
+@app.route("/chaining")
+def chaining():
+  """Step 7.
+  Dry run snippet and link metadata upload. """
+  return render_template("chaining.html")
+
+
+@app.route("/chaining/upload")
+def ajax_upload_link():
+  """Ajax upload link metadata. """
+  pass
+
+
+@app.route("/wrap-up")
+def wrap_up():
+  """Step 8.
+  Explain what to do with generated layout.
+   - Download layout
+   - Create project owner key (keygen snippet)
+   - Sign layout (signing snippet)
+   - Per functionary commands (in-toto-run snippet)
+   - Release instructions ??
+  """
+  return render_template("wrap_up.html")
+
+
+@app.route("/guarantees")
+def guarantees():
+  """ Show what the software supply chain protects against and give advice for
+  more guarantees. """
+  return render_template("guarantees.html")
 
 if __name__ == "__main__":
   app.run()
