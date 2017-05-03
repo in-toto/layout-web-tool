65e20657e56b280a8aee9256708f8530e0d1c65b
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-05-03 14:32:19 -0400

    Finishes basic versioning page UI
    Basic versioning page consists of a grid for predefined options
    (currently dummy options) with radio buttons and a form (input
    field) that expands below the option.
    Additionally, there is a button to add a custom VCS commands
    (currently not limited to one - this should probably be changed).
    
    Expand/collapse and show/hide is performed using custom JS.
    
    The look and feel for this page probably won't change a lot but
    the code structure could be more elegant, (e.g. not one .row
    per "row").
    
    Also I went back to style using bootstrap classes in the HTML
    instead of extending custom classes in SCSS - while this makes
    the HTML more convoluted, it is easier for me during prototyping
    (trying out bootstrap classes).

---
+++ static/js/main.js
@@ -0,0 +1,51 @@
+$(function() {
+
+  // Initialize option form container collapsibles
+  // https://v4-alpha.getbootstrap.com/components/collapse/
+  $(".opt-form-cont").collapse({
+     toggle: false,
+  });
+
+  // Show/Hide/Toggle forms and toggle "active"class
+  $(".opt-cell").on("click", function(evt) {
+    var opt_form_cont = "#" + $(this).data("target");
+
+    // Hide all others
+    $(".opt-form-cont").not(opt_form_cont).collapse("hide")
+    $(".opt-cell").not(this).removeClass("active")
+
+    // If the checkbox is checked we always show (leave shown)
+    // and hide (leave hidden) if unchecked.
+    if (evt.target.type == "checkbox") {
+      if (evt.target.checked) {
+        $(opt_form_cont).collapse("show");
+        $(this).addClass("active");
+
+      } else {
+        $(opt_form_cont).collapse("hide");
+        $(this).removeClass("active");
+      }
+    // If something other than a checkbox is clicked we just toggle
+    } else {
+      $(opt_form_cont).collapse("toggle");
+      $(this).toggleClass("active");
+    }
+  });
+
+
+  $("#add-cmd").on("click", function(evt){
+    $(".opt-form.template")
+        .clone()
+        .appendTo("#custom-cmd-container")
+        .slideDown(function(){
+          $(this).removeClass("template");
+        })
+        
+  });
+
+  $(document).on("click", "button.rm-cmd", function(evt) {
+    $(this).closest(".opt-form").slideUp(function(){
+      $(this).remove();
+    });
+  });
+});
