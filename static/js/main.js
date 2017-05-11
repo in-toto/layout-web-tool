cc7bfe08a9e3b9ef56ab90ab5485e4c2a60cb714
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-05-11 11:19:36 -0400

    Adds not working D3 force-directed graph
    - JS data transformation that turns in-toto layout into data
      suitable for D3 graphs (will probably move transformation to server)
    - A mostly copy pasted, not yet working D3 froce directed graph
      (will probably use a different library, e.g. dagre-d3)

--- static/js/main.js
+++ static/js/main.js
@@ -69,8 +69,82 @@ $(function() {
    * FIXME: modularize don't use data from global variable `layout_data`
    * (c.f. software_supply_chain.html)
    */
-
-  console.log(layout_data);
-  // var svg = d3.select("body")
-
+  draw_graph(layout_data);
 });
+
+
+
+
+var draw_graph = function(layout_data) {
+
+  // Transform data
+  var nodes = [], links = [];
+  ["steps", "inspect"].forEach(function(item_type) {
+    if (item_type in layout_data) {
+      layout_data[item_type].forEach(function(item){
+
+        var item_name = item.name;
+
+        nodes.push({
+          id: item_name,
+          type: item._type
+        });
+
+        ["material_matchrules", "product_matchrules"].forEach(function(artifact_rules) {
+          if (artifact_rules in item) {
+            item[artifact_rules].forEach(function(rule) {
+              if (rule[0].toLowerCase() == "match") {
+                links.push({
+                  source: item_name,
+                  target: rule[rule.length - 1]
+                });
+              }
+            });
+          }
+        });
+      });
+    }
+  });
+
+  var svg = d3.select("svg-content");
+  var simulation = d3.forceSimulation()
+      .force("charge", d3.forceManyBody())
+      .force("link", d3.forceLink().id(function(d) { return d.id; }))
+      .force("center", d3.forceCenter());
+
+  var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(links)
+      .enter()
+      .append("line");
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(nodes)
+      .enter()
+      .append("circle")
+      .attr("r", 5);
+
+  node.append("title")
+    .text(function(d) {
+      return d.id;
+    });
+
+  simulation.nodes(nodes)
+    .on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; })
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+
+  simulation.force("link").links(links);
+  
+}
+
+
