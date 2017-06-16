75af093f88d0509a52197953d1e3eb9790d3f97e
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-06-16 10:33:23 -0400

    Remove opt checkbox + case handling in form toggle
    The checkbox/radio in option cells are removed due to some
    UI changes (see following commits).
    As a consequence we don't have to treat check/uncheck differently
    in the option grid form toggle click listener.

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,6 @@ $(function() {
   /*
    * Click listener to toggle option form in the option grid and toggle active
    * class.
-   * Cases:
-   * Form is shown (stays shown) if checkbox is checked
-   * Form is hidden (stays hidden) if checkbox is unchecked
-   * Form is toggled if click occurs outside of checkbox in the option cell
    */
   $(".opt-content").on("click", function(evt) {
     var $opt_form_cont = $(this).parent(".opt-cell").find(".opt-form-cont");
@@ -14,22 +10,8 @@ $(function() {
     $(".opt-form-cont").not($opt_form_cont).slideUp();
     $(".opt-content").not(this).removeClass("active");
 
-    // If the checkbox is checked we always show (leave shown)
-    // and hide (leave hidden) if unchecked.
-    if (evt.target.type == "checkbox") {
-      if (evt.target.checked) {
-        $opt_form_cont.slideDown();
-        $(this).addClass("active");
-
-      } else {
-        $opt_form_cont.slideUp();
-        $(this).removeClass("active");
-      }
-    // If something other than a checkbox is clicked we just toggle
-    } else {
-      $opt_form_cont.slideToggle();
-      $(this).toggleClass("active");
-    }
+    $opt_form_cont.slideToggle();
+    $(this).toggleClass("active");
   });
 
   /*
