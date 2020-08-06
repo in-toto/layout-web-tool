a3241e33f189cee4f7b5955e880ca3cc18e2694f
Ray Chen <yc3346@nyu.edu>
2020-08-06 14:36:33 +0530

    Add function to generate artifact rules.
    Adds a function to before_after_filesystem_snapshot.py called
    generate_artifact_rules.

--- before_after_filesystem_snapshot.py
+++ before_after_filesystem_snapshot.py
@@ -25,3 +25,40 @@ def snapshot(before_dict, after_dict):
   # Returning the snapshot of the new file system
   return (sorted(unchanged_files), sorted(modified_files), sorted(added_files),
   sorted(removed_files))
+
+def generate_artifact_rules(snapshot):
+  '''
+  Generate Artifact Rules given which files have been added, which have been
+  removed, which have been modified, and which have remained unchanged.
+  '''
+  expected_materials = []
+  expected_products = []
+
+  # TODO: missing rules for MATCH since we don't have the information of the
+  # material from the previous step
+  for file in snapshot[0]:
+    # unchanged files
+    expected_materials.append(["ALLOW", file])
+  for file in snapshot[1]:
+    # modified files
+    expected_materials.append(["ALLOW", file])
+  for file in snapshot[3]:
+    # removed files
+    expected_materials.append(["DELETE", file])
+  expected_materials.append(["DISALLOW", "*"])
+
+  for file in snapshot[0]:
+    # unchanged files
+    expected_products.append(["ALLOW", file])
+  for file in snapshot[1]:
+    # modified files
+    expected_products.append(["MODIFY", file])
+  for file in snapshot[2]:
+    # added files
+    expected_products.append(["CREATE", file])
+  expected_products.append(["DISALLOW", "*"])
+
+  return {
+    'expected_materials': expected_materials,
+    'expected_products': expected_products
+  }
